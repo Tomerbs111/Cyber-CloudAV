ab7672ae3e4302052db426f642e095fc2f71126e
תומר בן שושן <shoonshoon1@outlook.com>
2023-12-17 20:25:35 +0200

    testing

--- GUI/reg_gui.py
+++ GUI/reg_gui.py
@@ -5,26 +5,29 @@ import re
 class RegistrationApp(CTk):
     def __init__(self):
         super().__init__()
-        self.toplevel_window = None
         self.geometry("700x500")
         self.title("Registration App")
 
+        self.registered_email = None
+        self.registered_username = None
+        self.registered_password = None
+
         # ----------------welcome----------------
         set_appearance_mode("system")
-        welcome = CTkLabel(
+        self.welcome = CTkLabel(
             master=self,
             text="Welcome to CloudAV",
             font=("Calibri bold", 35)
         )
-        welcome.place(relx=0.02, rely=0.05)
+        self.welcome.place(relx=0.02, rely=0.05)
 
         # ----------------email----------------
-        l_email = CTkLabel(
+        self.l_email = CTkLabel(
             master=self,
             text="Email",
             font=("Calibri", 15)
         )
-        l_email.place(relx=0.02, rely=0.15)
+        self.l_email.place(relx=0.02, rely=0.15)
         self.email_entry = CTkEntry(
             master=self,
             placeholder_text="example@example.com",
@@ -38,12 +41,12 @@ class RegistrationApp(CTk):
         self.ans_email.place(relx=0.02, rely=0.255)
 
         # ----------------username----------------
-        l_username = CTkLabel(
+        self.l_username = CTkLabel(
             master=self,
             text="Username",
             font=("Calibri", 15)
         )
-        l_username.place(relx=0.02, rely=0.3)
+        self.l_username.place(relx=0.02, rely=0.3)
         self.username_entry = CTkEntry(
             master=self,
             placeholder_text="Min. 6 characters",
@@ -57,12 +60,12 @@ class RegistrationApp(CTk):
         self.ans_username.place(relx=0.02, rely=0.405)
 
         # ----------------password----------------
-        l_password = CTkLabel(
+        self.l_password = CTkLabel(
             master=self,
             text="Password",
             font=("Calibri", 15)
         )
-        l_password.place(relx=0.02, rely=0.45)
+        self.l_password.place(relx=0.02, rely=0.45)
         self.password_entry = CTkEntry(
             master=self,
             placeholder_text="Min. 8 characters",
@@ -77,28 +80,29 @@ class RegistrationApp(CTk):
         self.ans_password.place(relx=0.02, rely=0.555)
 
         # ----------------register----------------
-        register_btn = CTkButton(
+        self.submit_btn = CTkButton(
             master=self,
             text="Register",
             command=self.when_submit
         )
-        register_btn.place(relx=0.02, rely=0.65)
+        self.submit_btn.place(relx=0.02, rely=0.65)
         self.l_confirm = CTkLabel(
             master=self,
             text="",
             font=("Calibri bold", 20),
             text_color="#00ff00"
         )
-        self.l_confirm.place(relx=0.02, rely=0.75)
         self.login_btn = CTkButton(
             master=self,
             text="Wanna log in?",
             command=self.go_to_login
         )
         self.login_btn.place(relx=0.25, rely=0.65)
+        self.l_confirm.place(relx=0.02, rely=0.75)
 
     def when_submit(self):
         checksum = 0
+
         u_email = self.email_entry.get()
         if len(u_email) > 0 and re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', u_email):
             self.ans_email.configure(text="Email is valid.", text_color="#00ff00")
@@ -108,12 +112,18 @@ class RegistrationApp(CTk):
             checksum -= 1 if checksum != 0 else 0
 
         u_username = self.username_entry.get()
-        if len(u_username) >= 6:
-            self.ans_username.configure(text="Username is valid", text_color="#00ff00")
-            checksum += 1
+        if  self.username_entry:
+            if len(u_username) >= 6:
+                self.ans_username.configure(text="Username is valid", text_color="#00ff00")
+                checksum += 1
+            else:
+                self.ans_username.configure(text="Invalid username. must be 6 characters or longer.",
+                                            text_color="#FF0000")
+                checksum -= 1 if checksum != 0 else 0
         else:
-            self.ans_username.configure(text="Invalid username. must 6 characters or longer.", text_color="#FF0000")
-            checksum -= 1 if checksum != 0 else 0
+            checksum += 1
+            pass
+            # in the login page
 
         u_password = self.password_entry.get()
         if len(u_password) > 0 and len(u_password) >= 8:
@@ -125,97 +135,27 @@ class RegistrationApp(CTk):
 
         if checksum == 3:
             self.l_confirm.configure(text="User Registered successfully")
+            self.registered_email = u_email
+            self.registered_username = u_username
+            self.registered_password = u_password
 
     def go_to_login(self):
+        self.welcome.configure(text="Log into CloudAV")
+        self.submit_btn.configure(text="Log in", command=self.when_submit)
 
+        self.l_username.destroy()
+        self.username_entry.destroy()
+        self.ans_username.destroy()
 
+        # Shift the password-related widgets up
+        self.l_password.place(rely=0.3)
+        self.password_entry.place(rely=0.35)
+        self.ans_password.place(rely=0.405)
 
-class LoginApp(CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("700x500")
-        self.title("Login App")
+        # Adjust the position of other widgets
+        self.login_btn.place(rely=0.5)
+        self.submit_btn.place(rely=0.5)
+        self.l_confirm.place(rely=0.6)
 
-        # ----------------welcome----------------
-        set_appearance_mode("system")
-        welcome = CTkLabel(
-            master=self,
-            text="Welcome back!",
-            font=("Calibri bold", 35)
-        )
-        welcome.place(relx=0.02, rely=0.05)
-
-        # ----------------email----------------
-        l_email = CTkLabel(
-            master=self,
-            text="Email",
-            font=("Calibri", 15)
-        )
-        l_email.place(relx=0.02, rely=0.15)
-        self.email_entry = CTkEntry(
-            master=self,
-            placeholder_text="example@example.com",
-            width=300,
-        )
-        self.email_entry.place(relx=0.02, rely=0.2)
-        self.ans_email = CTkLabel(
-            master=self,
-            text=""
-        )
-        self.ans_email.place(relx=0.02, rely=0.255)
-
-        # ----------------password----------------
-        l_password = CTkLabel(
-            master=self,
-            text="Password",
-            font=("Calibri", 15)
-        )
-        l_password.place(relx=0.02, rely=0.3)
-        self.password_entry = CTkEntry(
-            master=self,
-            placeholder_text="Min. 8 characters",
-            width=300,
-            show="●"
-        )
-        self.password_entry.place(relx=0.02, rely=0.35)
-        self.ans_password = CTkLabel(
-            master=self,
-            text=""
-        )
-        self.ans_password.place(relx=0.02, rely=0.405)
-
-        # ----------------login----------------
-        login_btn = CTkButton(
-            master=self,
-            text="Login",
-            command=self.when_login
-        )
-        login_btn.place(relx=0.02, rely=0.5)
-        self.l_confirm = CTkLabel(
-            master=self,
-            text="",
-            font=("Calibri bold", 20),
-            text_color="#00ff00"
-        )
-        self.l_confirm.place(relx=0.02, rely=0.65)
-
-    def when_login(self):
-        checksum = 0
-        u_email = self.email_entry.get()
-        if len(u_email) > 0 and re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', u_email):
-            self.ans_email.configure(text="Email is valid.", text_color="#00ff00")
-            checksum += 1
-        else:
-            self.ans_email.configure(text="Invalid email. Please enter a valid email.", text_color="#FF0000")
-            checksum -= 1 if checksum != 0 else 0
-
-        u_password = self.password_entry.get()
-        if len(u_password) > 0 and len(u_password) >= 8:
-            self.ans_password.configure(text="Password is valid", text_color="#00ff00")
-            checksum += 1
-        else:
-            self.ans_password.configure(text="Invalid password. must be 8 characters or longer.", text_color="#FF0000")
-            checksum -= 1 if checksum != 0 else 0
-
-        if checksum == 2:
-            self.l_confirm.configure(text="User Registered successfully")
+    def get_user_values(self):
+        return self.registered_email, self.registered_username, self.registered_password
--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from GUI.reg_gui import *
 if __name__ == "__main__":
     reg = RegistrationApp()
     reg.mainloop()
+    u_email, u_username, u_password = reg.get_user_values()
+    if u_email is not None and u_username is not None and u_password is not None:
+        print("Email:", u_email)
+        print("Username: ", u_username)
+        print("Password:", u_password)
+
+
+
