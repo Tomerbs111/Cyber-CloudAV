cb5fbe5504e6eceb134e15f63c089ed9e0eafc85
תומר בן שושן <shoonshoon1@outlook.com>
2024-01-02 16:15:56 +0200

    added class to manage files

---
+++ database/UserFiles.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+class UserFiles:
+    def __init__(self, userid, database_path='../database/User_info.db'):
+        self.conn = sqlite3.connect(database_path)
+        self.cur = self.conn.cursor()
+        self.userid = userid
+
+    def InsertFile(self, userid, name, size, date, filebytes):
+        insert_file = '''
+        INSERT INTO UserFiles (UserID, Name, Size, Date, FileBytes)
+        VALUES (?, ?, ?, ?, ?);
+        '''
+        self.cur.execute(insert_file, (self.userid, name, size, date, filebytes))
+        self.conn.commit()  # Don't forget to commit the changes
+
+    def RemoveFile(self, name):
+        remove_file = '''
+        DELETE FROM UserFiles WHERE UserID = ? AND Name = ?;
+        '''
+        self.cur.execute(remove_file, (self.userid, name,))
+        self.conn.commit()  # Don't forget to commit the changes
+
+    # You might want to add a method to close the connection when you're done
+    def close_connection(self):
+        self.conn.close()
+
+
