b3ea889401a142dbf5266a5d7838754e8dc777e3
תומר בן שושן <shoonshoon1@outlook.com>
2024-02-01 20:37:52 +0200

    added shit ton of icons

--- GUI/MainPage.py
+++ GUI/MainPage.py
@@ -8,6 +8,7 @@ import ttkbootstrap as ttk
 from customtkinter import *
 from PIL import Image, ImageTk
 
+
 class FileFrame(ttk.Frame):
     def __init__(self, master, fname, fsize, fdate):
         super().__init__(master)
@@ -15,11 +16,16 @@ class FileFrame(ttk.Frame):
         self.fsize = fsize
         self.fdate = fdate
 
+        self.check_var = StringVar(value="off")
+        self.mark_for_action = ttk.Checkbutton(self, text="",
+                                               variable=self.check_var, onvalue="on", offvalue="off")
+        self.mark_for_action.pack(side='left', padx=5)
+
         lu_filename = ttk.Label(
             master=self,
             text=self.fname
         )
-        lu_filename.pack(side='left', padx=30)
+        lu_filename.pack(side='left', padx=5)
 
         lu_size = ttk.Label(
             master=self,
@@ -32,11 +38,6 @@ class FileFrame(ttk.Frame):
             text=self.fdate
         )
         lu_date_mod.pack(side='right', padx=43)
-
-        self.check_var = customtkinter.StringVar(value="off")
-        self.mark_for_action = ttk.Checkbutton(self, text="",
-                                               variable=self.check_var, onvalue="on", offvalue="off")
-        self.mark_for_action.pack(side='left')
 
     def get_checkvar(self) -> bool:
         return self.check_var.get() == "on"
@@ -57,18 +58,18 @@ class MainPage(ttk.Frame):
 
         # Initialize instance variables
         self.f_data_center = None
-        self.f_file_tags = None
+        self.f_action = None
         self.f_file_list = None
         self.file_frames = []  # List to store FileFrame instances
         self.file_frame_counter = 0
         self.save_path = None
 
         # Call the setup functions
-        self.setup_action_frame()
+        self.setup_searchbar_frame()
         self.setup_data_center_frame()
         self.setup_option_frame()
-        self.setup_file_tags_frame()
-        self.setup_file_list_frame()
+        self.setup_action_frame()
+        self.setup_file_management_frame()
 
         narf_thread = threading.Thread(target=self.notify_and_receive_files)
         narf_thread.start()
@@ -82,44 +83,201 @@ class MainPage(ttk.Frame):
 
             self.add_file_frame(short_filename, formatted_file_size, short_file_date)  # a func from Gui_CAV.py
 
+    def setup_searchbar_frame(self):
+        # Code for setting up the Searchbar frame
+        f_searchbar = ttk.Frame(master=self, style="dark")
+        f_searchbar.place(relx=0, rely=0, relheight=0.1, relwidth=1)
 
+        # Logo Placeholder on the Left
+        cloudav_image = CTkImage(
+            light_image=Image.open(
+                r"../GUI/file_icons/only_logo.png"),
+            dark_image=Image.open(
+                r"../GUI/file_icons/only_logo.png"),
+            size=(75, 75))
+        cav_image_lbl = CTkLabel(master=f_searchbar, image=cloudav_image, text="").pack(side="left", padx=10)
 
-    def setup_action_frame(self):
-        # Code for setting up the Action frame
-        f_actions = ttk.Frame(master=self)
-        f_actions.place(x=0, y=0, relheight=0.1, relwidth=1)
-        ttk.Button(self, text="Logout").pack(expand=True, fill='both')
+        # Search Entry in the Center
+        search_entry = ttk.Entry(f_searchbar, width=60)  # Adjust the width as needed
+        search_entry.pack(side="left", fill="both", expand=True, padx=(0, 5))  # Reduced padx
+
+        # Search Button in the Center
+        search_button = ttk.Button(f_searchbar, text="Search")
+        search_button.pack(side="left", padx=5)  # Reduced padx
+
+        # Profile Photo Placeholder on the Right
+        profile_photo_placeholder = ttk.Label(f_searchbar, text="👤", font=("Arial", 14))
+        profile_photo_placeholder.pack(side="right", padx=10)
+
+        # Settings Button on the Right
+        settings_button = ttk.Button(f_searchbar, text="Settings")
+        settings_button.pack(side="right", padx=10)
+
+        # Center the entry widget vertically
+        f_searchbar.grid_propagate(False)
+        f_searchbar.update_idletasks()
+        search_entry.place(in_=f_searchbar, anchor="center", relx=0.5, rely=0.5)
 
     def setup_data_center_frame(self):
         # Code for setting up the Data center frame
-        self.f_data_center = ttk.Frame(master=self)
+        self.f_data_center = ttk.Frame(master=self, style="dark")
         self.f_data_center.place(rely=0.1, x=0, relheight=0.9, relwidth=1)
+
+    # ... (Previous code)
 
     def setup_option_frame(self):
         # Code for setting up the Option frame
-        f_options = ttk.Frame(master=self.f_data_center)
-        f_options.place(x=0, y=0, relwidth=0.2, relheight=1)
-        ttk.Label(master=f_options, text="f_options").pack(expand=True, fill='both')
+        f_options = ttk.Frame(master=self, style="dark")
+        f_options.place(relx=0, rely=0.1, relwidth=0.2, relheight=1)
 
-    def setup_file_tags_frame(self):
+        CTkButton(
+            f_options,
+            text="Add",
+            command=self.add_file,
+            image=CTkImage(Image.open("../GUI/file_icons/add_file_plus_icon.png"), size=(30, 30)),
+            compound='left'
+        ).pack(side='top', pady=20, anchor='w', padx=10)
+
+        ttk.Separator(f_options, orient="horizontal").pack(side='top', fill='x', pady=5, padx=10)
+
+        CTkButton(
+            f_options,
+            text="Home",
+            image=CTkImage(Image.open("../GUI/file_icons/home_icon.png"), size=(20, 20)),
+            compound='left'
+        ).pack(side='top', pady=5, anchor='w', fill='x', padx=10)
+
+        CTkButton(
+            f_options,
+            text="Shared",
+            image=CTkImage(Image.open("../GUI/file_icons/shared_icon.png"), size=(20, 20)),
+            compound='left'
+        ).pack(side='top', pady=5, anchor='w', fill='x', padx=10)
+
+        CTkButton(
+            f_options,
+            text="Favorites",
+            image=CTkImage(Image.open("../GUI/file_icons/star_icon.png"), size=(20, 20)),
+            compound='left'
+        ).pack(side='top', pady=5, anchor='w', fill='x', padx=10)
+
+        CTkButton(
+            f_options,
+            text="Groups",
+            image=CTkImage(Image.open("../GUI/file_icons/group_icon.png"), size=(20, 20)),
+            compound='left'
+        ).pack(side='top', pady=5, anchor='w', fill='x', padx=10)
+
+        CTkButton(
+            f_options,
+            text="Recycle bin",
+            image=CTkImage(Image.open("../GUI/file_icons/trash_icon.png"), size=(20, 20)),
+            compound='left'
+        ).pack(side='top', pady=5, anchor='w', fill='x', padx=10)
+
+        CTkButton(
+            f_options,
+            text="Log out",
+            image=CTkImage(Image.open("../GUI/file_icons/log_out_icon.png"), size=(20, 20)),
+            compound='left'
+        ).pack(side='top', pady=5, anchor='w', fill='x', padx=10)
+
+        ttk.Label(f_options, text="Storage:").pack(side='top', pady=10, anchor='w', fill='x', padx=10)
+
+    def setup_action_frame(self):
         # Code for setting up the File tags frame
-        self.f_file_tags = ttk.Frame(master=self.f_data_center)
-        self.f_file_tags.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.13)
+        self.f_action = ttk.Frame(master=self.f_data_center)
+        self.f_action.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.05)
 
-        add_file_btn = (ttk.Button(master=self.f_file_tags, text="add file", command=self.add_file)
-                        .pack(fill='x'))
+        # Delete Button
+        delete_button = CTkButton(
+            master=self.f_action,
+            image=CTkImage(Image.open("../GUI/file_icons/trash_icon.png"), size=(20, 20)),
+            compound='left',
+            text="Delete",
+            width=30,
+            fg_color='transparent'
+        )
+        delete_button.pack(side='left', padx=5)
 
-        process_checked_file_frames = (ttk.Button(master=self.f_file_tags, text="get results",
-                                                  command=self.receive_checked_files).pack(fill='x'))
+        # Download Button
+        download_button = CTkButton(
+            master=self.f_action,
+            command=self.receive_checked_files,
+            image=CTkImage(Image.open("../GUI/file_icons/download_icon.png"), size=(20, 20)),
+            compound='left',
+            text="Download",
+            width=30,
+            fg_color='transparent'
+        )
+        download_button.pack(side='left', padx=5)
 
-    def setup_file_list_frame(self):
-        # Code for setting up the File list frame
-        self.f_file_list = ScrolledFrame(master=self.f_data_center, autohide=True, bootstyle='solar')
-        self.f_file_list.place(relx=0.2, rely=0.13, relwidth=0.8, relheight=0.87)
+        # Rename Button
+        rename_button = CTkButton(
+            master=self.f_action,
+            image=CTkImage(Image.open("../GUI/file_icons/rename_icon.png"), size=(20, 20)),
+            compound='left',
+            text="Rename",
+            width=30,
+            fg_color='transparent'
+        )
+        rename_button.pack(side='left', padx=5)
 
-        ttk.Label(master=self.f_file_tags, text="Name").pack(side='left', padx=30)
-        ttk.Label(master=self.f_file_tags, text="Size").pack(side='right', padx=50)
-        ttk.Label(master=self.f_file_tags, text="Upload date").pack(side='right', padx=25)
+        # Shared Button
+        shared_button = CTkButton(
+            master=self.f_action,
+            image=CTkImage(Image.open("../GUI/file_icons/shared_icon.png"), size=(20, 20)),
+            compound='left',
+            text="Share",
+            width=30,
+            fg_color='transparent'
+        )
+        shared_button.pack(side='left', padx=5)
+
+        # Copy Button
+        copy_button = CTkButton(
+            master=self.f_action,
+            image=CTkImage(Image.open("../GUI/file_icons/copy_icon.png"), size=(20, 20)),
+            compound='left',
+            text="Copy",
+            width=30,
+            fg_color='transparent'
+        )
+        copy_button.pack(side='left', padx=5)
+
+        # Favorite Button
+        favorite_button = CTkButton(
+            master=self.f_action,
+            image=CTkImage(Image.open("../GUI/file_icons/star_icon.png"), size=(20, 20)),
+            compound='left',
+            text="Favorite",
+            width=30,
+            fg_color='transparent'
+        )
+        favorite_button.pack(side='left', padx=5)
+
+    # ...
+
+    def setup_file_management_frame(self):
+        f_file_management = ttk.Frame(master=self.f_data_center)
+        f_file_management.place(relx=0.2, rely=0.05, relwidth=0.8, relheight=0.95)
+
+        f_file_properties = ttk.Frame(master=f_file_management)
+        f_file_properties.place(relx=0, rely=0, relwidth=1, relheight=0.05)
+
+        CTkButton(master=f_file_properties, text="Name").pack(side='left', padx=5)
+        CTkButton(master=f_file_properties, text="Size").pack(side='right', padx=27)
+        CTkButton(master=f_file_properties, text="Upload date").pack(side='right', padx=47)
+
+        self.f_file_list = ScrolledFrame(master=f_file_management, autohide=False)
+        self.f_file_list.place(relx=0, rely=0.05, relwidth=1, relheight=0.95)
+
+    def setup_file_properties_frame(self):
+        f_file_properties = CTkFrame(master=self.f_data_center, corner_radius=10)
+        f_file_properties.place(relx=0.2, rely=0.05, relwidth=0.79, relheight=0.05)
+
+        self.f_file_list = CTkScrollableFrame(master=self.f_data_center, corner_radius=10)
+        self.f_file_list.place(relx=0.2, rely=0.1, relwidth=0.79, relheight=0.88)
 
     # client communication parts in GUI
     def add_file(self):
@@ -143,9 +301,8 @@ class MainPage(ttk.Frame):
                 self.prepare_for_display(file_name, file_bytes, file_date)
 
             send_file_thread = threading.Thread(
-                target=self.client_communicator.send_file(file_name, short_filename, formatted_file_size,
-                                                          short_file_date,
-                                                          file_bytes))
+                target=self.client_communicator.send_file(file_name, short_filename,
+                                                          short_file_date, formatted_file_size,file_bytes))
             send_file_thread.start()
 
             self.add_file_frame(short_filename, formatted_file_size, short_file_date)
@@ -241,8 +398,8 @@ class MainPage(ttk.Frame):
         return checked_file_frames_list
 
     def get_save_path_dialog(self):
-        dialog = customtkinter.CTkInputDialog(text="Write the path you want to save your files on:",
-                                              title="Get save path")
+        dialog = CTkInputDialog(text="Write the path you want to save your files on:",
+                                title="Get save path")
         input_path = dialog.get_input()
 
         if input_path:
@@ -251,3 +408,28 @@ class MainPage(ttk.Frame):
         else:
             # Use the default Downloads folder
             self.save_path = os.path.join(os.path.expanduser("~"), "Downloads")
+
+
+class MyApp(ttk.Window):
+    def __init__(self, client_communicator):
+        super().__init__(themename="darkly")
+        self.geometry("1150x710")
+        self.title("Cloud-AV")
+
+        self.client_communicator = client_communicator
+        self.current_frame = None
+        self.switch_frame(MainPage, self.client_communicator)
+
+    def switch_frame(self, frame_class, *args):
+        new_frame = frame_class(self, self.switch_frame, *args)
+
+        if self.current_frame:
+            self.current_frame.pack_forget()
+
+        new_frame.pack(fill="both", expand=True)
+        self.current_frame = new_frame
+
+
+if __name__ == "__main__":
+    my_app = MyApp("1")
+    my_app.mainloop()
